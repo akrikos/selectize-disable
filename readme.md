4ed654bcbea44303db45474604f086e723d1dd28
Mark Merrill <akrikos@akrikos.net>
2014-03-17 23:05:13 -0400

    Added customizable field name and option to inverse which field is disabled

--- readme.md
+++ readme.md
@@ -1,6 +1,21 @@
 A plugin for allowing disabling of options for with selectize.
 
-When creating options in selectize add an entry 'disabled: true' to disable an individual option like this:
+TODO: make these instructions more user friendly
+
+To install:
+
+1. Add selectize-disable to your bower configuration
+1. include src/selectize-disable.css and src/selectize-disable.js on your page or in your build process
+1. add the selectize-disable to your selectize options for the element you want to use
+    $("#demo").selectize({
+      plugins: {
+        'option-disable': {fieldName: 'matchesRule', inverse: false}
+      }
+    })
+1. ...
+1. Profit!
+
+When creating options in selectize add an entry 'disabled: true' (default) or specify your own field name to disable an individual option:
 
     {
         text: 'Example Display Value',
--- src/selectize-disabled.js
+++ src/selectize-disabled.js
@@ -1,11 +1,26 @@
-Selectize.define('option-disable', function() {
+Selectize.define('option-disable', function(options) {
+  console.log(options);
+  var inverseValue = false,
+    fieldName = 'disabled';
+
+  if (options && typeof options.inverse !== 'undefined') {
+    inverseValue = !!options.inverse
+  }
+
+  if (options && typeof options.fieldName !== 'undefined') {
+    fieldName = options.fieldName
+  }
+
   // override render to show which elements are disabled
   var originalRender = this.render;
   this.render = function(templateName, data) {
+    console.log('render', data.name, data[fieldName], options.inverse, data[fieldName] ^ options.inverse);
     var html = originalRender.apply(this, arguments);
-    if (data.disabled)
+    if (data[fieldName] ^ options.inverse) {
+      console.log('disabled', data);
       // first instance of option surrounded by quotes or spaces should be the class
       html = html.replace(/(['" ])option(?=['" ])/, '$1option selectize-disabled');
+    }
     return html;
   }
 
